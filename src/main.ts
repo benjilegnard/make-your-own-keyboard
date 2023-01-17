cf7160bd263dba2af10167f0e8b679888ba7bcaf
Benjamin Legrand <benjilegnard@protonmail.com>
2023-01-17 11:07:39 +0100

    chore: add reveal types

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 
 import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown';
 
 let deck = new Reveal({
    plugins: [ Markdown ]
