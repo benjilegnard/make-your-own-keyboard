7976c43d9bf1223a9a5bd2b137b52fbdcfa338c4
Benjamin Legrand <benjilegnard@protonmail.com>
2023-01-18 15:44:43 +0100

    chore: add hash navigation in url, add notes plugin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import './style.css'
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
+import "./style.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
 
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown';
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown";
+import Notes from "reveal.js/plugin/notes/notes";
 
 let deck = new Reveal({
-   plugins: [ Markdown ]
-})
-deck.initialize();
+  plugins: [Markdown, Notes],
+});
+
+deck.initialize({
+  progress: false,
+  controls: true,
+  hashOneBasedIndex: true,
+  hash: true,
+  history: true,
+});
