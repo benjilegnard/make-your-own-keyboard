83b48b5561069979e26a92c3811dddb918717c45
Benjamin Legrand <benjilegnard@protonmail.com>
2023-03-11 12:39:27 +0100

    style: use white theme, setup custom scss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import "./style.css";
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/black.css";
+import "./style.scss";
 
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown";
